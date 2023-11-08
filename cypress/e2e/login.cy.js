2e327c9176fe2c039396ecb2f2d003e0d96f16ba
zalacop <103320914+zalacop@users.noreply.github.com>
2023-11-08 22:01:26 +0100

    cypress logout test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -20,5 +20,13 @@ describe('Login test', () => {
   });
 });
 
-
+describe('Logout test', () => {
+  it("User can log out", () => {
+    cy.visit('/index.html');
+    cy.wait(1000);
+    cy.get("header button[type=button]").contains("Logout").click;
+    cy.wait(1000);
+    cy.getAllLocalStorage().should("be.empty");
+  });
+})
 
