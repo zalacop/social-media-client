086ddcc4a65a24fe072c5740721c62da69fc20be
zalacop <103320914+zalacop@users.noreply.github.com>
2023-11-09 15:09:56 +0100

    updated cypress logout test

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,9 +1,20 @@
+const email = "twix@noroff.no";
+const password = "twixtwix";
+
 describe('Logout test', () => {
     it("User can log out", () => {
       cy.visit('/index.html');
       cy.wait(1000);
-      cy.get("header button[type=button]").contains("Logout").click;
+      cy.get("#registerModal").contains("Login").click();
+      cy.get("#loginForm").should("be.visible");
       cy.wait(1000);
-      cy.getAllLocalStorage().should("be.empty");
+      cy.get("#loginEmail").type(email);
+      cy.get("#loginPassword").type(password);
+      cy.get("button[type=submit]").contains("Login").click();
+      cy.wait(2000);
+      cy.get("header button[type=button]").contains("Logout").click();
+      cy.wait(1000);
+      cy.clearLocalStorage();
+      cy.window().its('localStorage').should('be.empty');
     });
   })
--- cypress/e2e/validation.cy.js
+++ cypress/e2e/validation.cy.js
@@ -9,33 +9,33 @@ describe("Form validation test", () => {
         cy.wait(1000);
         cy.get("#registerModal").contains("Login").click();
         cy.wait(1000);
-      });
-
-      it("User cannot log in with wrong email", () => {
+    });
+        
+    it("User cannot log in with wrong email", () => {
         cy.get("#loginEmail").type(wrongEmail);
         cy.get("#loginPassword").type(password);
         cy.get("#loginForm button[type=submit]").contains("Login").click();
         cy.wait(2000);
-      });
-
-      it("User cannot log in with wrong password", () => {
+    });
+        
+    it("User cannot log in with wrong password", () => {
         cy.get("#loginEmail").type(email);
         cy.get("#loginPassword").type(wrongPassword);
         cy.get("#loginForm button[type=submit]").contains("Login").click();
         cy.wait(2000);
-      });
+    });
 
-      it("User cannot log in without filling out the form", () => {
-        cy.get('#loginEmail:invalid').should('exist'); 
-        cy.get('#loginPassword:invalid').should('exist'); 
-        cy.get("#loginForm button[type=submit]").contains("Login").click();
-        cy.wait(2000);
-      });
-      
-      it("User cannot login with wrong email and password", () => {
+    it("User cannot login with wrong email and password", () => {
         cy.get("#loginEmail").type(wrongEmail);
         cy.get("#loginPassword").type(wrongPassword);
         cy.get("#loginForm button[type=submit]").contains("Login").click();
         cy.wait(2000);
-    })
+    });
+
+    it("User cannot log in without filling out the form", () => {
+        cy.get('#loginEmail:invalid').should('exist'); 
+        cy.get('#loginPassword:invalid').should('exist'); 
+        cy.get("#loginForm button[type=submit]").contains("Login").click();
+        cy.wait(2000);
+    });
 })
